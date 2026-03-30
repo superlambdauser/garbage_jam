a3163a813b07a5de6241b66ff2346599cac7a4bd
deepthroadocus <sockpuppetmastaz@proton.me>
2026-03-30 14:04:40 +0200

    main loop

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,25 @@
-import pygame
+import pygame as pg
+
+# pygame setup
+pg.init()
+screen = pg.display.set_mode((1600, 1200))
+clock = pg.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    pg.display.flip() # Update screen 
+
+    clock.tick(60)  # FPS limit = 60
+
+pg.quit()
